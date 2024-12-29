21c06264b5c4d005d5dd4868f55060e50317474e
Abhilash Mandula <mandula.abhilash@gmail.com>
2024-12-29 23:11:42 +0530

    Give a shadow to the charts cards

--- components/admin/tasks/charts/TaskChartCard.js
+++ components/admin/tasks/charts/TaskChartCard.js
@@ -4,7 +4,7 @@ import { Card } from "@/components/ui/card";
 
 export const TaskChartCard = ({ title, children }) => {
   return (
-    <Card className="p-4">
+    <Card className="p-4 shadow-lg">
       <h3 className="font-medium mb-4">{title}</h3>
       <div className="w-full aspect-[4/3]">{children}</div>
     </Card>
