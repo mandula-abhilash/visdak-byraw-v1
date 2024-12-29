fc89a952a18604bcfef744cfd352d2b7d8d1384b
Abhilash Mandula <mandula.abhilash@gmail.com>
2024-12-29 15:25:16 +0530

    Fix missing logo in mobile nav

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -2,6 +2,7 @@
 
 import { DesktopNav } from "./Navigation/DesktopNav";
 import { MobileNav } from "./Navigation/MobileNav";
+import { Logo } from "./Logo";
 
 export const Layout = ({ children }) => {
   return (
@@ -9,7 +10,8 @@ export const Layout = ({ children }) => {
       <DesktopNav />
       <div className="flex-1 flex flex-col min-h-screen">
         <div className="md:hidden sticky top-0 z-50 border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-          <div className="flex h-14 items-center justify-end px-4">
+          <div className="flex h-14 items-center justify-between px-4">
+            <Logo />
             <MobileNav />
           </div>
         </div>
